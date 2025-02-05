be51906e76c1c94955a9d5ce8f8d0fc88b28f209
kon5271220000 <kon527122000@gmail.com>
2025-02-05 16:50:48 +0700

    create to do item construction

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,15 @@
-import { greeting } from "./greeting";
+import { toDoItem } from "./to-do-item"
 import "../style.css"
 
-console.log(greeting)
+const exampleItem = new toDoItem("buy milk", "27-12-2025", "emergence", "buy vinamilk", "not done")
+
+const content = document.getElementById("content")
+
+const titleItem = document.createElement("p")
+titleItem.textContent = exampleItem.title
+content.appendChild(titleItem)
+
+const dueDateItem = document.createElement("p")
+dueDateItem.textContent = exampleItem.dueDate
+content.appendChild(dueDateItem)
+
---
+++ src/js/to-do-item.js
@@ -0,0 +1,8 @@
+export const toDoItem = function(title, description, dueDate, priority, notes, checklist){
+    this.title = title
+    this.description = description
+    this.dueDate = dueDate
+    this.priority = priority
+    this.notes = notes
+    this.checklist = checklist
+}
